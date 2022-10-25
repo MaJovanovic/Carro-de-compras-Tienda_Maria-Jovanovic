09700393a1f24db58837bcd9f2f350f7fb792cab
MaJovanovic <110514002+MaJovanovic@users.noreply.github.com>
2022-10-25 19:00:21 -0300

    sumaprod

--- main.js
+++ main.js
@@ -1,49 +1,44 @@
-let cart = document.querySelectorAll('.add-cart');
-let product = [
-    {
-        nombre: 'teclado',
-        tag: 'tec',
+let cart = document.querySelectorAll(".add-cart");
+let product = [{
+        nombre: "teclado",
+        tag: "tec",
         precio: 1000,
-        inCart: 0
+        inCart: 0,
     },
     {
-        nombre: 'mouse',
-        tag: 'mou',
+        nombre: "mouse",
+        tag: "mou",
         precio: 600,
-        inCart: 0
+        inCart: 0,
     },
     {
-        nombre: 'pantalla',
-        tag: 'pant',
+        nombre: "pantalla",
+        tag: "pant",
         precio: 2500,
-        inCart: 0
-    }
-]
+        inCart: 0,
+    },
+];
 
-console.log("carro " , cart);
-console.log("tamaño carro ",cart.length);
-console.log("prueba producto ",product[0]);
+console.log("carro ", cart);
+console.log("tamaño carro ", cart.length);
+console.log("prueba producto ", product[0]);
 
-for (let i=0; i< cart.length; i++) {
-    cart[i].addEventListener('click', () => {
-        console.log(product[i].nombre ,"aa");
+for (let i = 0; i < cart.length; i++) {
+    cart[i].addEventListener("click", () => {
+        console.log(product[i].nombre, "aa");
         cartNumbers(product[i]);
-        
-    })
+    });
 }
 
 function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem ('cartNumbers');
+    let productNumbers = localStorage.getItem("cartNumbers");
 
     if (productNumbers) {
-        document.querySelector('.cart span').textContent = productNumbers;
+        document.querySelector(".cart span").textContent = productNumbers;
     }
 }
 
-
-
 function cartNumbers(product) {
-
     let productNumbers = localStorage.getItem("cartNumbers");
     productNumbers = parseInt(productNumbers);
     console.log(productNumbers);
@@ -51,42 +46,35 @@ function cartNumbers(product) {
     console.log(JSON.stringify(product));
 
     if (productNumbers) {
-        localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector('.cart span').textContent = productNumbers + 1;
+        localStorage.setItem("cartNumbers", productNumbers + 1);
+        document.querySelector(".cart span").textContent = productNumbers + 1;
     } else {
-        localStorage.setItem('cartNumbers', 1);
-        document.querySelector('.cart span').textContent = 1;
+        localStorage.setItem("cartNumbers", 1);
+        document.querySelector(".cart span").textContent = 1;
     }
-    localStorage.setItem("productsInCart", JSON.stringify(product));
-
+    //localStorage.setItem("productsInCart", JSON.stringify(product));
 
     setItems(product);
-
 }
 
-
 function setItems(product) {
-
-    let cartItems = JSON.parse(localStorage.getItem('productsInCart'));
+    let cartItems = JSON.parse(localStorage.getItem("productsInCart"));
 
     if (cartItems != null) {
         if (cartItems[product.tag] == undefined) {
             cartItems = {
                 ...cartItems,
                 [product.tag]: product,
-            }
-            cartItems[product.tag].inCart +=1;
-        } else {
-            product.inCart = 1;
-            cartItems = {
-                //...cartItems,
-                [product.tag]: product
-            }
+            };
         }
-        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product,
+        };
     }
-
-    console.log("items en el carro ", cartItems);
-
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
+
 onLoadCartNumbers();
